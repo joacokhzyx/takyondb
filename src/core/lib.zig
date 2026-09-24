b82a7c1fdf0af72d011ebd5cec966f192ed69122
Joaco <joaquindheredia@sanluis.edu.ar>
2026-09-24 22:31:01 +0000

    feat(core): expose relational barrel from lib
    Signed-off-by: Joaco <joaquindheredia@sanluis.edu.ar>

--- src/core/lib.zig
+++ src/core/lib.zig
@@ -15,3 +15,4 @@ pub const ring_buffer = @import("ipc/ring_buffer.zig");
 pub const wal = @import("storage/wal.zig");
 pub const recovery = @import("storage/recovery.zig");
 pub const art = @import("index/art.zig");
+pub const relational = @import("relational/lib.zig");
