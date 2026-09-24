59addd5ebc38b24bbda8867a833d152cbcb7b227
Joaco <joaquindheredia@sanluis.edu.ar>
2026-09-24 22:29:25 +0000

    feat(sdk): export relational barrel
    Signed-off-by: Joaco <joaquindheredia@sanluis.edu.ar>

---
+++ src/sdk/client/relational/index.ts
@@ -0,0 +1,25 @@
+/**
+ * ============================================================================
+ * File: index.ts
+ * Description: Public barrel for the relational SDK.
+ * Author/Maintainer: TakyonDB Contributors
+ * License: MIT. See LICENSE for details.
+ * ============================================================================
+ */
+
+export * from './types';
+export * from './column';
+export * from './errors';
+export * from './utils';
+export * from './schema';
+export * from './codec';
+export * from './filter';
+export * from './aggregation';
+export * from './table';
+export * from './database';
+export * from './query';
+export * from './join';
+export * from './transaction';
+export * from './secondary_index';
+export * from './sql';
+export * from './executor';
